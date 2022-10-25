7b81ae91d1de2a2f1cfda1f82a502aca074d8972
Sebastian Igescu <sebastian.igescu@bearingpoint.com>
2022-10-25 17:21:47 +0300

    next: section 12, unit 105

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -15,17 +15,50 @@
 
 
 // THIS IS DONE BY PROMISES
-const doWorkPromise = new Promise( (resolve, reject) => {
-    setTimeout( () => {
-        // resolve( [7, 4, 1] ); // this will apply to .then()
-        reject( 'Things went wrong' ); // this will apply to .catch()
-    }, 2000 );
-})
+// const doWorkPromise = new Promise( (resolve, reject) => {
+//     setTimeout( () => {
+//         // resolve( [7, 4, 1] ); // this will apply to .then()
+//         reject( 'Things went wrong' ); // this will apply to .catch()
+//     }, 2000 );
+// })
 
-doWorkPromise
-    .then( (result) => {
-        console.log( 'Success!', result );
+// doWorkPromise
+//     .then( (result) => {
+//         console.log( 'Success!', result );
+//     })
+//     .catch( (error) => {
+//         console.log( 'Error:', error );
+//     });
+
+
+// PROMISE CHAINING
+const add = (a,b) => {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve(a+b)
+        }, 2000)
     })
-    .catch( (error) => {
-        console.log( 'Error:', error );
-    });
+}
+
+// bad way. This is not chaining.
+// add(1, 2)   .then( sum => {
+//                 console.log( sum )
+
+//                 add( sum, 5 )   .then( sum2 => console.log(sum2) )
+//                                 .catch( e => console.log(e) )
+//             })
+//             .catch( error => {
+//                 console.log( error );
+//             })
+
+
+// real promise chaining
+add(1,1)
+    .then( sum => {
+        console.log(sum)
+        return add(sum, 4)
+    }).then( sum2 => {
+        console.log(sum2)
+    }).catch( error => {
+        console.log(error)
+    })
