bf45f203ddbe7a9a4e8c6b0d8a1edb9b81383876
Sebastian Igescu <sebastian.igescu@bearingpoint.com>
2022-10-29 18:07:19 +0300

    next: section 17 unit 160

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,1 +1,38 @@
-io()
+const socket = io()
+const messageInput = document.querySelector('#message-input')
+const messageBtn = document.querySelector('#message-submit')
+const sharelocBtn = document.querySelector('#sharelocation')
+
+socket.on('message', (msg) => {
+    console.log(msg)
+})
+
+messageBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    socket.emit('sendMessage', messageInput.value, (cb_error) => {
+        if (cb_error) return console.log(cb_error)
+
+        console.log('Message delivered!')
+    })
+    messageInput.value=''
+})
+
+socket.on('sendMessage', msg => {
+    console.log(msg)
+})
+
+sharelocBtn.addEventListener('click', (e) => {
+    if (!navigator.geolocation) return alert('Geolocation is not supported by your browser...')
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position)
+
+        const loc = {
+            lat: position.coords.latitude, 
+            long: position.coords.longitude
+        }
+        socket.emit('sendLocation', loc, () => {
+            console.log('Location shared successfully!')
+        })
+    })
+})
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -3,6 +3,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 //------------------------------------
 const app = express()
 const server = http.createServer(app)
@@ -15,8 +16,31 @@ const publicDirectoryPath = path.join(__dirname, "../public")
 app.use(express.static(publicDirectoryPath));
 //------------------------------------
 
-io.on('connection', () => {
+// let count = 0
+
+io.on('connection', (socket) => {
     console.log('New WebSocket connection')
+
+    socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('message', 'A new user has joined!')
+
+    socket.on('sendMessage', (msg, callback) => {
+        const filter = new Filter()
+        if (filter.isProfane(msg)) return callback('Error: Profanity is not allowed!')
+
+        io.emit('sendMessage', msg)
+        callback()
+    })
+
+    socket.on('sendLocation', ({lat, long}, callback) => {
+        io.emit('message', `https://google.com/maps?q=${lat},${long}`)
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        /* socket has already left, so will receive nothing */ // socket.broadcast.emit('message', 'An user has left!')
+        io.emit('message', 'An user has left!')
+    })
 })
 
 server.listen(port, () => {
